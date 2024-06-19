fc5bcc4aa5c6feb46737643f851715e23df2014b
Mithun Param <paramatm@mcmaster.ca>
2024-06-18 20:56:53 -0400

    can edit/update todo items now

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-const EditTodoForm = () => {
+const EditTodoForm = ({editTodo, task}) => { // pass editTodo function as a prop to update the list item
+    const [value, setValue] = useState(task.task); // state hook to manage user input, set initially to the todo task provided as a prop
+
+    const handleChange = event => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault(); // prevent page from reloading when form is submitted
+        editTodo(value, task.id);
+        setValue('');
+    }
+
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='Update selected task:' onChange={handleChange}/>
+        <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
   )
 }
 
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
 import { v4 as uuidv4 } from 'uuid';
+import EditTodoForm from './EditTodoForm';
 uuidv4(); // use uuidv4 to generate new ids for list items
 
 const TodoWrapper = () => {
@@ -20,8 +21,12 @@ const TodoWrapper = () => {
       setItems(items.filter(item => item.id !== id)) // keep the items that do not have the id we want to delete
     }
 
-    const editItem = id => {
+    const editItem = id => { // update the isEditing attribute of the todo item so the EditTodoForm component will be rendered instead of the regular Todo
       setItems(items.map(item => item.id === id ? {...item, isEditing: !item.isEditing} : item))
+    }
+
+    const editTodo = (value, id) => { // update the value of the task that we want to edit and change isEditing attribute back to false
+      setItems(items.map(item => item.id === id ? {...item, task: value, isEditing: !item.isEditing} : item))
     }
 
   return (
@@ -31,8 +36,14 @@ const TodoWrapper = () => {
         <TodoForm createItem={createItem} /> 
 
         {/* Todo item components to display tasks */}
-        {items.map((item, index) => ( 
-          <Todo task={item} key={index} handleClick={handleClick} deleteItem={deleteItem} editItem={editItem} />
+        {/* If we want to edit a todo item, display the EditTodoForm component, otherwise display the regular Todo component */}
+        {items.map((item, index) => (
+          item.isEditing ? ( 
+            <EditTodoForm editTodo={editTodo} task={item} />
+          ) :
+          (
+            <Todo task={item} key={index} handleClick={handleClick} deleteItem={deleteItem} editItem={editItem} />
+          )      
         ))}
     </div>
   )
